aa596ca0315bdfb8c05378449758e9d9dd195ea7
Gianni Chen <giannikchen@gmail.com>
2012-01-14 17:10:26 -0500

    adding directories

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <link href="css/layout.css" rel="stylesheet" type="text/css"/>
+  <?php include('php/functions.php'); ?>
+  <title><?php course_title(); ?></title>
+</head>
+
+<body>
+  <div class="container">
+    <h1 class="course-title"><?php course_title(); ?></h1>
+    <?php nav(); ?>
+
+    <h2 class="page-title">Course Information</h2>
+    <?php get_info(); ?>
+
+    <h2 class="page-title">Annoucements</h2>
+    <?php get_announcements(); ?>
+  </div>
+</body>
+
+
+</html>
---
+++ php/functions.php
@@ -0,0 +1,96 @@
+<?php
+function nav() {
+         echo "<nav>";
+              echo "<a href='index.php'>Home</a>";
+              echo "<a href='projects.php'>Projects</a>";
+              echo "<a href='exams.php'>Exams</a>";
+         echo "</nav>";
+}
+
+function course_title() {
+         $info = 'course.info';
+         if (file_exists($info) && is_readable($info)) {
+            if (($info_fh = fopen($info, 'r'))) {
+                  $line = fgets($info_fh);
+                  $toks = explode(": ", $line);
+                  if ($toks[0] == "Course Code") {
+                     echo $toks[1];
+                  }
+                  $line = fgets($info_fh);
+                  $toks = explode(": ", $line);
+                  if ($toks[0] == "Course Name") {
+                     echo " - ".$toks[1];
+                  }
+                  fclose($info_fh);
+            }
+         } else {
+           echo "Your course";
+         }
+}
+
+function get_info() {
+         $info = 'course.info';
+         if (file_exists($info) && is_readable($info)) {
+            if (($info_fh = fopen($info, 'r'))) {
+               echo "<p>";
+               while (!feof($info_fh)) {
+                     $line = fgets($info_fh);
+                     $toks = explode(": ", $line);
+                     if ($toks[0] == "Course Code" || $toks[0] == "Course Name") {
+                        continue;
+                     } else if ($toks[0] == "Description") {
+                        echo $toks[0].":<br/>".$toks[1]."<br/>";
+                     } else {
+                        echo $line."<br/>";
+                     }
+               }
+               echo "</p>";
+               fclose($info_fh);
+            }
+         }
+}
+
+function get_announcements() {
+         $announce_file= 'announcements.info';
+         if (file_exists($announce_file) && is_readable($announce_file)) {
+            if (($announce_fh = fopen($announce_file, 'r'))) {
+               while (!feof($announce_fh)) {
+                     $line = fgets($announce_fh);
+                     echo $line."<br/>";
+                     $line = fgets($announce_fh); //prone to breaking
+                     echo $line."<br/><br/>";
+               }
+               fclose($announce_fh);
+            }
+         }
+
+}
+
+function get_directory($basedir) {
+         if (file_exists($basedir) && is_dir($basedir)) {
+            if (($base_dh = opendir($basedir))) {
+               while (($entry_dir = readdir($base_dh)) !== false) {
+                     if (is_dir($basedir."/".$entry_dir)) {
+                        if ($entry_dir == ".." || $entry_dir == ".") {
+                           continue; //do not include parent and self
+                        }
+
+                        if (($dir_dh = opendir($basedir."/".$entry_dir))) {
+                           echo $entry_dir."<br/>";
+                           while (($entry = readdir($dir_dh)) !== false) {
+                              if ($entry == ".." || $entry == ".") {
+                                 continue;
+                              }
+                              echo "<a href='".$basedir."/".$entry_dir."/".$entry."'>".$entry."</a><br/>";
+                           }
+                           echo "<br/>";
+                           closedir($dir_dh);
+                        }
+                     }
+               }
+               closedir($base_dh);
+            }
+         }
+}
+
+?>
