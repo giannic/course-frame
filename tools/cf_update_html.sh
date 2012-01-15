b61bbf786f351be1c4b587c048c8dfc42b7dc8a8
Gianni Chen <giannikchen@gmail.com>
2012-01-15 17:13:11 -0500

    naive update html script

--- tools/cf_update_html.sh
+++ tools/cf_update_html.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+cp php_pages/* .
+rm index.html exams.html projects.html
 php index.php > index.html
 php exams.php > exams.html
 php projects.php > projects.html
-mkdir -p php_pages
-mv index.php exams.php projects.php php_pages/
+rm index.php exams.php projects.php
